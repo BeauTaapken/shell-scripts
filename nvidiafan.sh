4761911783c4fb9e30590d0984f81b42ec2d950c
Beau <beau@lioncode.nl>
2024-02-10 09:12:25 +0100

    added a fan control script for nvidia

---
+++ nvidiafan.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+# Change "j" for your sudo password
+source ./.env
+
+# Function
+setfan() {
+  xhost si:localuser:root
+  echo $password | sudo -S /usr/bin/nvidia-settings -a "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1" -a "*:1[fan-1]/GPUTargetFanSpeed=$1"
+  xhost -si:localuser:root
+}
+
+# Change 'setfan' parameter for speed % and 'X' intervals for temperature boundaries
+for(( ; ; ))
+do
+X=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+if ((0<=X && X<=25))
+then
+  setfan 20
+elif ((36<=X && X<=50))
+then
+  setfan 40
+elif ((51<=X && X<=100))
+then
+  setfan 100
+fi
+
+sleep 10
+done
