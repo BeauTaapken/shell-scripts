dcde28c25af9d162b085127953a6d215c61b9993
Beau <beau@lioncode.nl>
2024-03-13 16:39:17 +0100

    updated script to at least check if the value before the . is a number, nvidia really has to fuck up to make this break (shouldn't give any other issues, i hope) gp

--- nvidia-overclock.sh
+++ nvidia-overclock.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+
 source /mnt/crucial-ssd/shell-scripts/.env
 
 wattage=$(nvidia-smi -q -d POWER | grep 'Max Power Limit' | grep ' W' | sed -z 's/Max Power Limit                   : //g; s/ W//g;')
 
-echo $password | sudo -S nvidia-smi -pl $wattage
+# Convert string to int, float isn't supported by bash
+wattageAsInt=$((${wattage%.*} + 0))
+
+# if wattage is a number
+if [[ $wattageAsInt =~ '^[0-9]+$' ]] ; then
+    echo $password | sudo -S nvidia-smi -pl $wattage
+fi
