9986fb4b5a454ac802564bb264a6841d13281005
Beau <beau@lioncode.nl>
2024-06-18 16:44:27 +0200

    changes to cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-~='/home/beau'
+cd /home/beau/.config/nvim/
+/usr/bin/git add .
+/usr/bin/git commit -m "automatic push"
+/usr/bin/git push
 
 config='/usr/bin/git --git-dir=/home/beau/.cfg/ --work-tree=/home/beau'
 
 $config add -u
 $config commit -m "automatic push"
 $config push -u origin main
-
-cd ~/.config/nvim/
-/usr/bin/git add .
-/usr/bin/git commit -m "automatic push"
-/usr/bin/git push -u origin main
